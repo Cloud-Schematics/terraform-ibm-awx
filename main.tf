1cc374abdb1c26f6c353696a8224c51db79985b7
epradeepk <38468365+epradeepk@users.noreply.github.com>
2019-12-17 14:54:00 +0530

    Update

--- main.tf
+++ main.tf
@@ -1,9 +1,10 @@
 # This file is used to install AWX and it's prerequisites on VSI
 
 provider "ibm" {
-  generation       = 1
+  generation   = 2
   region = "${var.region}"
 }
+
 
 # Module to create Infrastructure
 module vpc_pub_priv {
@@ -13,7 +14,8 @@ module vpc_pub_priv {
   vpc_name = "${var.vpc_name}"
   subnet_zone = "${var.subnet_zone}"
   ssh_keyname = "${var.ssh_keyname}"
-  ssh_public_key = "${var.ssh_public_key}"
+  ssh_keyname = "${ibm_is_ssh_key.public_sshkey.id}"
+  ssh_yes_or_no = "${var.ssh_yes_or_no}"
 }
 
 # Resource to execute awx_install.sh script on VSI
@@ -22,11 +24,12 @@ resource "null_resource" "awxinstall" {
     type = "ssh"
     user = "root"
     host = "${module.vpc_pub_priv.floating_ip_address}"
-    private_key = "${var.ssh_private_key}"
+    private_key = "${file(local.ssh_privatekey_file)}"
   }
   provisioner "remote-exec" {
     script = "awx_install.sh"
   }
+  depends_on = ["null_resource.create_ssh_keys"]
 }
 
 output "awxaccess" {
@@ -36,3 +39,32 @@ output "awxaccess" {
 output "sshcommand" {
  value = "ssh command => ssh root@${module.vpc_pub_priv.floating_ip_address}"
 }
+
+resource "ibm_is_ssh_key" "public_sshkey" {
+  name = "${var.ssh_keyname}"
+  public_key = "${file(local.ssh_publickey_file)}"
+  depends_on = ["null_resource.create_ssh_keys"]
+}
+
+locals {
+  ssh_publickey_file = "sshkey_id_rsa.pub"
+}
+
+locals {
+  ssh_privatekey_file = "sshkey_id_rsa"
+}
+
+resource "null_resource" "create_ssh_keys" {
+  provisioner "local-exec" {
+    command = "ssh-keygen -q -t rsa -N '' -f sshkey_id_rsa <<< y;chmod 600 sshkey_id_rsa*"
+    interpreter = ["/bin/bash", "-c"]
+  }
+}
+
+output "resource_cloud" {
+   value = {
+            "resource_controller_url" = "Access AWX via browser http://${module.vpc_pub_priv.floating_ip_address}"
+             "resource_name" = "AWX"
+  }
+}
+
